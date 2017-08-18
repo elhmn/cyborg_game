c13dedc412e8feb757e4ac0f3f9d0a417046982e
elhmn <elhmn@localhost.localdomain>
2017-08-18 19:55:10 +0200

    je suis con

--- server/incs/server.h
+++ server/incs/server.h
@@ -1,4 +1,53 @@
 #ifndef SERVER_H
 # define SERVER_H
 
+# include <wslay/wslay.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <poll.h>
+# include <signal.h>
+
+# define SIGRT_CLOSE	(SIGRTMIN)
+
+typedef struct		s_com
+{
+	int		sock;
+	int		pid;
+}					t_com;
+
+typedef struct		s_env
+{
+	t_com			com_tab[4];
+
+	//parent to child pipe
+	int				ptoc_pipe[2]; 
+
+	//child to parent pipe
+	int				ctop_pipe[4][2];
+
+}					t_env;
+
+/*
+ ** wslay_callback.c
+ */
+
+int 				make_non_block(int fd);
+ssize_t 			send_callback(wslay_event_context_ptr ctx,
+					const uint8_t *data, size_t len, int flags,
+					void *user_data);
+ssize_t 			recv_callback(wslay_event_context_ptr ctx, uint8_t *buf, size_t len,
+					int flags, void *user_data);
+void				on_msg_recv_callback(wslay_event_context_ptr ctx,
+					const struct wslay_event_on_msg_recv_arg *arg,
+					void *user_data);
+
+/*
+** tools.c
+*/
+char**				str_split(char* a_str, const char a_delim);
+char 				*cvtInt(char *str, int num);
+
+void				free_tab(char ***tab);
+void				show_tab(char **tab);
+
 #endif
--- server/srcs/main.c
+++ server/srcs/main.c
@@ -4,12 +4,16 @@
 #include <unistd.h>
 #include <errno.h>
 #include <netdb.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include "server.h"
+#include "websocket.h"
+
 
 int					put_socket_ipv4(int sock)
 {
@@ -101,10 +105,62 @@ int		create_socket_stream_ipv4(char *host_name,
 	return (sock);
 }
 
-int		communication_handler(int sock)
+# define BUFSIZE	1024
+
+void				pipe_com_write(int fd, const char *msg)
 {
-	socklen_t				len;
-	struct sockaddr_in		addr;
+	unsigned char	buf[BUFSIZE];
+
+	if (msg && fd > 0)
+	{
+		strncpy((char*)buf, (const char*)msg, BUFSIZE);
+		fprintf(stdout, "buf = [%s]\n", buf);//_DEBUG_//
+		if (write(fd, buf, BUFSIZE) != BUFSIZE)
+		{
+			perror("write");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+unsigned char		*pipe_com_read(int fd)
+{
+	unsigned char	*buf;
+
+	if (!(buf = (unsigned char*)malloc(sizeof(unsigned char) * BUFSIZE)))
+	{
+		fprintf(stdout, "Error : malloc");
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	memset(buf, 0, BUFSIZE);
+	if (read(fd, buf, BUFSIZE) != BUFSIZE && errno != EAGAIN)
+	{
+		perror("read");
+ 		exit(EXIT_FAILURE);
+	}
+	fprintf(stdout, "buf = [%s]\n", buf);//_DEBUG_//
+	return (buf);
+}
+
+
+
+int					communication_handler(t_env env, int idx, int sock)
+{
+	wslay_event_context_ptr 		ctx;
+	struct wslay_event_callbacks 	callbacks = {
+													recv_callback,
+													send_callback,
+													NULL,
+													NULL,
+													NULL,
+													NULL,
+													on_msg_recv_callback
+												};
+	struct Session 					session = {sock};
+	struct pollfd					event;
+	socklen_t						len;
+	struct sockaddr_in				addr;
 
 	len = sizeof(struct sockaddr_in);
 	if (getpeername(sock, (struct sockaddr*)&addr, &len) < 0)
@@ -114,6 +170,55 @@ int		communication_handler(int sock)
 	}
  	fprintf(stdout, "Peer : ");
 	put_addr((struct sockaddr*)&addr, len);
+	if (http_handshake(sock) < 0)
+	{
+		perror("http_handshake");
+		return (-1);
+	}
+	if (make_non_block(sock) < 0)
+	{
+		perror("make_non_block");
+		return (-1);
+	}
+	memset(&event, 0, sizeof(struct pollfd));
+	event.fd = sock;
+	event.events = POLLIN;
+	wslay_event_context_server_init(&ctx, &callbacks, &session);
+	while(wslay_event_want_read(ctx) || wslay_event_want_write(ctx))
+	{
+		if(poll(&event, 1, -1) == -1)
+		{
+			perror("poll");
+			return (-1);
+		}
+		if(((event.revents & POLLIN) && wslay_event_recv(ctx) != 0)
+			|| ((event.revents & POLLOUT) && wslay_event_send(ctx) != 0)
+			|| (event.revents & (POLLERR | POLLHUP | POLLNVAL)))
+		{
+			(void)env;
+			(void)idx;
+			//send close message
+// 			pipe_com_write(env.ctop_pipe[idx][1], "con");
+
+			/*
+			 * If either wslay_event_recv() or wslay_event_send() return
+			 * non-zero value, it means serious error which prevents wslay
+			 * library from processing further data, so WebSocket connection
+			 * must be closed.
+			 */
+			perror("wslay_event_recv or send Fatal");
+			return (-1);
+		}
+		event.events = 0;
+		if(wslay_event_want_read(ctx))
+		{
+			event.events |= POLLIN;
+		}
+		if(wslay_event_want_write(ctx))
+		{
+			event.events |= POLLOUT;
+		}
+	}
 	close(sock);
 	return (0);
 }
@@ -124,13 +229,96 @@ int					quit_server(void)
 	return (0);
 }
 
-int		connection_handler(int sock)
+
+void				put_comtab(t_com *tab, int s)
 {
+	int		i;
+
+	for (i = 0; i < s; i++)
+	{
+		fprintf(stdout, "(%d, %d)", tab[i].pid, tab[i].sock);
+		if (i != s - 1)
+		{
+			fprintf(stdout, "; ");
+		}
+	}
+	fprintf(stdout, "\n");
+}
+
+void				init_comtab(t_com *tab, int s)
+{
+	int		i;
+
+	for (i = 0; i < s; i++)
+	{
+		tab[i].sock = -1;
+		tab[i].pid = -1;
+	}
+}
+
+// void				signal_handler(int signal)
+// {
+// 	if (signal == SIGRT_CLOSE)
+// 	{
+// 		fprintf(stdout, "signal close\n");
+// 	}
+// }
+int					choose_sock_idx(t_env e, int idx)
+{
+	int		i;
+
+	if (e.com_tab[idx].sock == -1)
+		return (idx);
+	else
+	{
+		for (i = 0; i < 4; i++)
+		{
+			if (e.com_tab[i].sock == -1)
+				return (i);
+		}
+	}
+	return (idx);
+}
+
+int					isfull(t_env e)
+{
+	int		i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (e.com_tab[i].sock == -1)
+			return (0);
+	}
+	return (1);
+}
+
+int					connection_handler(int sock)
+{
+	t_env				env;
 	int					sock_com;
+	int					full;
+	int					i;
+	int					idx;
+
+	char				**tab;
+	unsigned char		*buftmp;
+	char				msg[BUFSIZE];
 	socklen_t			len;
 	struct sockaddr_in	addr;
 
+	full = 0;
+	idx = 0;
+	buftmp = NULL;
 	len = sizeof(struct sockaddr_in);
+	init_comtab(env.com_tab, 4);
+
+	if (pipe(env.ptoc_pipe) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	make_non_block(env.ptoc_pipe[0]);
+	make_non_block(env.ptoc_pipe[1]);
 	//inform kernel that we want to receive connection on that socket
 	if (listen(sock, 5) < 0)
 	{
@@ -139,33 +327,114 @@ int		connection_handler(int sock)
 	}
 	while (!quit_server())
 	{
-		memset(&addr, 0, len);
-		if ((sock_com = accept(sock, (struct sockaddr*)&addr, &len)) < 0)
+		if (!isfull(env))
 		{
-			perror("accept");
-			return (-1);
-		}
-		switch (fork())
-		{
-			//child
-			case 0:
-				close(sock);
-				communication_handler(sock_com);
-				exit(EXIT_SUCCESS);
-			case -1:
-				perror("fork");
+			put_comtab(env.com_tab, 4);//_DEBUG_//
+			memset(&addr, 0, len);
+			fprintf(stdout, "bef main : idx = [%d]\n", idx);//_DEBUG_//
+			if ((sock_com = accept(sock, (struct sockaddr*)&addr, &len)) < 0)
+			{
+				perror("accept");
 				return (-1);
-			//father
-			default:
-				close(sock_com);
-// 				signal(SIGCHLD, SIG_IGN); /* that's dirty */
-				signal(SIGCHLD, SIG_DFL); /* that's dirty */
+			}
+			idx = choose_sock_idx(env, idx);
+			env.com_tab[idx].sock = sock_com;
+			put_comtab(env.com_tab, 4);//_DEBUG_//
+			fprintf(stdout, "main : idx = [%d]\n", idx);//_DEBUG_//
+			if (pipe(env.ctop_pipe[idx]) == -1)
+			{
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
+			make_non_block(env.ctop_pipe[idx][0]);
+			make_non_block(env.ctop_pipe[idx][1]);
+			switch (fork())
+			{
+				//child
+				case 0:
+					close(sock);
+
+					//child close ptoc input
+					close(env.ptoc_pipe[1]);
+
+					//child close ctop output
+					close(env.ctop_pipe[idx][0]);
+
+					communication_handler(env, idx, sock_com);
+
+					//send close message
+					strcpy(msg, "con:close:");
+					char		tmp[BUFSIZE];
+					strcat(msg, cvtInt(tmp, getpid()));
+					pipe_com_write(env.ctop_pipe[idx][1], msg);
+
+					fprintf(stdout, "idx = [%d]\n", idx);//_DEBUG_////_DEBUG_//
+// 					env.com_tab[idx] = -1;
+// 					idx;
+					fprintf(stdout, "closed ! index = [%d] \n", idx);//_DEBUG_//
+					close(env.ctop_pipe[idx][0]);
+					close(env.ctop_pipe[idx][1]);
+ 					exit(EXIT_SUCCESS);
+
+				case -1:
+					perror("fork");
+					return (-1);
+
+				//father
+ 				default:
+ 					close(sock_com);
+
+					//father close ptoc input
+					close(env.ptoc_pipe[0]);
+
+					//father close ctop output
+					close(env.ctop_pipe[idx][1]);
+	 				signal(SIGCHLD, SIG_IGN); /* that's dirty */
+			}
 		}
+		else
+		{
+			if (!full)
+			{
+				full = 1;
+				usleep(3000);
+				fprintf(stdout, "Host reached its limit and can't handle more than 4 clients!\n");
+			}
+		}
+		(void)tab;
+		(void)i;
+		(void)buftmp;
+		//father pipe com event handler
+ 		for (i = 0; i < 4; i++)
+ 		{
+			fprintf(stdout, "[%d]\n", i);
+			if (env.com_tab[i].sock == -1)
+				break ;
+ 			buftmp = pipe_com_read(env.ctop_pipe[i][0]);
+			fprintf(stdout, "je suis con\n");
+			(void)tab;
+			//parse message
+			if (buftmp)
+			{
+				tab = str_split((char*)buftmp, ':');
+				if (tab)
+				{
+					show_tab(tab);
+					if (tab[0] && !strcmp((const char*)tab[0], "con"))
+					{
+						env.com_tab[i].sock = -1;
+						fprintf(stdout, "Message get [%d]\n", i);
+					}
+					free(tab);
+				}
+				free(buftmp);
+			}
+ 		}
 	}
 	return (0);
 }
 
-int		main(int ac, char **av)
+int					main(int ac, char **av)
 {
 	int		sock_con;
 	char	*port;
