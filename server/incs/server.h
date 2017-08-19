a4f192b4e2d9a13b62b9f72e6bcdf4f6e097dc93
elhmn <elhmn@localhost.localdomain>
2017-08-19 16:30:55 +0200

    code clean up

--- server/incs/server.h
+++ server/incs/server.h
@@ -2,13 +2,23 @@
 # define SERVER_H
 
 # include <wslay/wslay.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <poll.h>
-# include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 # define SIGRT_CLOSE	(SIGRTMIN)
 # define MAXPLAYER		4
+# define BUFSIZE		1024
 
 typedef struct		s_com
 {
@@ -27,6 +37,8 @@ typedef struct		s_env
 	//child to parent pipe
 	int				ctop_pipe[MAXPLAYER][2];
 
+	char			msg[BUFSIZE];
+	char			con_list[BUFSIZE];
 }					t_env;
 
 /*
@@ -55,5 +67,56 @@ char**				str_split(char* a_str, const char a_delim);
 char 				*cvtInt(char *str, int num);
 void				free_tab(char ***tab);
 void				show_tab(char **tab);
+
+/*
+** child_to_parent.c
+*/
+
+void				check_deconnection(t_env *env);
+void				send_con_close(t_env *env, int idx);
+
+/*
+** parent_to_child.c
+*/
+
+void				update_con_list(t_env *env);
+
+/*
+** pipe.c
+*/
+
+void				pipe_com_write(int fd, const char *msg);
+unsigned char		*pipe_com_read(int fd);
+
+/*
+** socket.c
+*/
+
+
+int					put_addr(struct sockaddr *addr, socklen_t len);
+int					put_socket_ipv4(int sock);
+int					create_socket_stream_ipv4(char *host_name,
+					char *serv_name,
+					int port,
+					char *proto_name);
+
+/*
+** environement.c
+*/
+
+void				put_comtab(t_com *tab, int s);
+void				init_comtab(t_com *tab, int s);
+
+/*
+** communication_handler.c
+*/
+
+int					communication_handler(t_env env, int idx, int sock);
+
+/*
+** connection_handler.c
+*/
+
+int					connection_handler(int sock);
 
 #endif
